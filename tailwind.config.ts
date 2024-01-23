5ab3c0376075d294d60321a7650c30809779b50b
Alexander Forrest <alex@alexdev.se>
2024-01-23 14:18:27 +0100

    clearing up..

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,11 +23,6 @@ const config: Config = {
         didot: "var(--font-didot)",
         roboto: "var(--font-roboto)",
       },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
     },
   },
   plugins: [],
